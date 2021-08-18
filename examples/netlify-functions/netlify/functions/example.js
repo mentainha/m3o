cb30d2199c8f19cf7e489058513263af1396337d
Asim Aslam <asim@aslam.me>
2021-08-18 12:43:28 +0100

    add netlify functions example

---
+++ examples/netlify-functions/netlify/functions/example.js
@@ -0,0 +1,38 @@
+const { MICRO_API_TOKEN } = process.env;
+
+const m3o = require("@m3o/m3o-node");
+
+exports.handler = async function (event, context) {
+  if (!MICRO_API_TOKEN) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "no api key" }),
+    };
+  }
+
+  let body = JSON.parse(event.body);
+
+  try {
+    let response = await new m3o.Client({ token: MICRO_API_TOKEN }).call(
+      // @todo change this to the actual API and endpoint you want to call
+      "helloworld", // the name of the API
+      "Call", // the name of the endpoint
+      body
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (e) {
+    if (e && e.response && e.response.data && e.response.data.Detail) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: e.response.data.Detail }),
+      };
+    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "something went wrong" }),
+    };
+  }
+};
