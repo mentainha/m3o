9635011e6f2d66a38933c6e2421ee068851fd331
Asim Aslam <asim@aslam.me>
2021-10-11 14:16:10 +0100

    Update curl.sh

--- examples/helloworld/curl.sh
+++ examples/helloworld/curl.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -z $MICRO_API_TOKEN ]; then
+if [ -z $M3O_API_TOKEN ]; then
   echo "Missing MICRO_API_TOKEN"
   exit 1
 fi
 
-curl -H "Authorization: Bearer $MICRO_API_TOKEN" \
+curl -H "Authorization: Bearer $M3O_API_TOKEN" \
      -H "Content-Type: application/json" \
      -d '{"name": "John"}' \
      https://api.m3o.com/v1/helloworld/call
