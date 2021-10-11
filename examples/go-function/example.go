64a5377040c03f47a83bf2b9a17771c930741e74
Asim Aslam <asim@aslam.me>
2021-10-11 15:52:54 +0100

    add go function example

---
+++ examples/go-function/example.go
@@ -0,0 +1,24 @@
+package example
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type response struct {
+	Message string `json:"message"`
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(response{Message: string(d)}); err != nil {
+		w.WriteHeader(500)
+		return
+	}
+}
+
