8594509b6aedfe14c3c42554e4c71402948fedb2
Asim Aslam <asim@aslam.me>
2021-10-27 20:43:22 +0100

    fix copy script

--- scripts/publicapi.sh
+++ scripts/publicapi.sh
@@ -2,11 +2,9 @@
 
 # Copy publicapi.json files from micro/services to m3o/m3o/api
 
-pushd services
+cd ../services
 
 find . -name publicapi.json | while read file; do 
 	name=`echo $file | sed 's@./\(.*\)/publicapi.json@\1@g'`;
 	cp $file ../m3o/api/$name.json;
 done
-
-popd
