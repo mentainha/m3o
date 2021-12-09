5c1123b7a4e674e8cdb382c9a4dfa61f641db3a0
Asim Aslam <asim@aslam.me>
2021-12-09 13:07:24 +0000

    fix websocket example (#91)

--- cmd/client-gen/go_template.go
+++ cmd/client-gen/go_template.go
@@ -109,13 +109,20 @@ import(
 
 			fmt.Println(rsp)
 	}{{ end }}
-}
-`
+}`
 
-const curlExampleTemplate = `{{ $service := .service }}curl "https://api.m3o.com/v1/{{ $service.Name }}/{{ title .endpoint }}" \
--H "Content-Type: application/json" \
--H "Authorization: Bearer $M3O_API_TOKEN" \
--d '{{ tsExampleRequest $service.Name .endpoint $service.Spec.Components.Schemas .example.Request }}'`
+const curlExampleTemplate = `{{ $service := .service -}}
+{{- if isNotStream $service.Spec $service.Name $reqType -}}
+curl "https://api.m3o.com/v1/{{ $service.Name }}/{{ title .endpoint }}" \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $M3O_API_TOKEN" \
+  -d '{{ tsExampleRequest $service.Name .endpoint $service.Spec.Components.Schemas .example.Request }}'
+{{- end -}}
+{{- if isStream $service.Spec $service.Name $reqType -}}
+echo '{{ tsExampleRequest $service.Name .endpoint $service.Spec.Components.Schemas .example.Request }}' | \
+websocat -n -H="Authorization: Bearer $M3O_API_TOKEN" \
+wss://api.m3o.com/v1/{{ $service.Name }}/{{ title .endpoint }}
+{{- end}}`
 
 const goReadmeTopTemplate = `{{ $service := .service }}# {{ title $service.Name }}
 
