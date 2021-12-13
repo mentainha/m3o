ac15c1b4598d521f92e250da43a06d647295e6c2
Asim Aslam <asim@aslam.me>
2021-12-13 13:04:20 +0000

    Service updates

--- services/avatar/main.go
+++ services/avatar/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"github.com/m3o/m3o/services/avatar/handler"
+	pb "github.com/m3o/m3o/services/avatar/proto"
+	imagePb "github.com/m3o/m3o/services/image/proto"
+
 	"github.com/micro/micro/v3/service"
 	"github.com/micro/micro/v3/service/logger"
-	"github.com/m3o/m3o/services/function/handler"
-	pb "github.com/m3o/m3o/services/function/proto"
 )
 
 func main() {
 	// Create service
 	srv := service.New(
-		service.Name("function"),
+		service.Name("avatar"),
 		service.Version("latest"),
 	)
 
 	// Register handler
-	pb.RegisterFunctionHandler(srv.Server(), handler.NewFunction())
+	hdlr := handler.NewAvatar(imagePb.NewImageService("image", srv.Client()))
+	pb.RegisterAvatarHandler(srv.Server(), hdlr)
 
 	// Run service
 	if err := srv.Run(); err != nil {
