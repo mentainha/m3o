633711665982b0880d6c6ca386b0078dd5fd65cd
Asim Aslam <asim@aslam.me>
2021-12-16 16:51:25 +0000

    update function exampel (#99)

--- examples/go-function/example.go
+++ examples/go-function/example.go
@@ -6,18 +6,52 @@ import (
 	"net/http"
 )
 
+type request struct {
+	Name string `json:"name"`
+}
+
 type response struct {
 	Message string `json:"message"`
 }
 
 func Helloworld(w http.ResponseWriter, r *http.Request) {
-	d, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(500)
+	message := "Hello world!"
+
+	ct := r.Header.Get("Content-Type")
+
+	if ct == "application/json" {
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+
+		var req request
+
+		if err := json.Unmarshal(b, &req); err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		if len(req.Name) > 0 {
+			message = "Hello " + req.Name + "!"
+		}
+
+		if err := json.NewEncoder(w).Encode(response{Message: message}); err != nil {
+			w.WriteHeader(500)
+			return
+		}
+
 		return
 	}
-	if err := json.NewEncoder(w).Encode(response{Message: string(d)}); err != nil {
-		w.WriteHeader(500)
-		return
+
+	r.ParseForm()
+	name := r.Form.Get("name")
+
+	if len(name) > 0 {
+		message = "Hello " + name + "!"
 	}
+
+	w.Write([]byte(message))
 }
