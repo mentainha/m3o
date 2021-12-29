f9c48cb5111e3c516781dbfe46dcd58374a8dbf4
Asim Aslam <asim@aslam.me>
2021-12-29 13:37:14 +0000

    fix build

--- cmd/client-gen/go_template.go
+++ cmd/client-gen/go_template.go
@@ -29,10 +29,9 @@ import(
 )
 
 type {{ title $service.Name }} interface {
-{{ range $key, $req := $service.Spec.Components.RequestBodies }}{{ $reqType := requestType $key }}
-{{ $endpointName := requestTypeToEndpointName $key}}	{{ $endpointName }}(*{{ requestType $key }}) (*{{ requestTypeToResponseType $key }}{{ if isStream $service.Spec $service.Name $reqType }}Stream{{end}}, error)
-}
+{{ range $key, $req := $service.Spec.Components.RequestBodies }}{{ $reqType := requestType $key }}{{ $endpointName := requestTypeToEndpointName $key}}	{{ $endpointName }}(*{{ requestType $key }}) (*{{ requestTypeToResponseType $key }}{{ if isStream $service.Spec $service.Name $reqType }}Stream{{end}}, error)
 {{end}}
+}
 func New{{ title $service.Name }}Service(token string) *{{ title $service.Name }}Service {
 	return &{{ title $service.Name }}Service{
 		client: client.NewClient(&client.Options{
