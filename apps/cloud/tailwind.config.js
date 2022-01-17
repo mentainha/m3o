b7a0a637fbf5f083c75bf5659979c66d474f3b62
martinv678 <martinvirando@hotmail.co.uk>
2022-01-17 15:28:05 +0000

    Updated the font to match cloud next (#138)

--- apps/cloud/tailwind.config.js
+++ apps/cloud/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    fontFamily: {
+      sans: ['Open\\ Sans']
+    }
   },
   plugins: []
 }
