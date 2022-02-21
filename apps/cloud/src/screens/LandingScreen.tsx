24851f0328163c059760319254ecd9a03404b83d
Asim Aslam <asim@aslam.me>
2022-02-21 09:59:24 +0000

    change main info line

--- apps/cloud/src/screens/LandingScreen.tsx
+++ apps/cloud/src/screens/LandingScreen.tsx
@@ -7,7 +7,7 @@ export const LandingScreen: FC = () => {
     <section className="p-14 text-white">
       <h1 className="text-5xl font-bold mb-4">Welcome to M3O Cloud</h1>
       <p className="text-xl max-w-xl text-gray-400">
-        Manage your content on M3O with M3O Cloud!
+        Manage your apps, users and data all in one place
       </p>
       <div className="mt-10 grid grid-cols-3 gap-4">
         <ApiBox
