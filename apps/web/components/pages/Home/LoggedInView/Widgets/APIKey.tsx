76c5fc43ea3729aec35e5258921911eeaa82633c
Asim Aslam <asim@aslam.me>
2022-03-13 19:40:44 +0000

    personal key

--- apps/web/components/pages/Home/LoggedInView/Widgets/APIKey.tsx
+++ apps/web/components/pages/Home/LoggedInView/Widgets/APIKey.tsx
@@ -9,7 +9,7 @@ export const APIKey: FC<Props> = ({ apiToken }) => {
   return (
     <div className="tbgc p-6 md:p-10 rounded-lg">
       <h5 className="font-bold text-xl text-black dark:text-white">
-        Your Personal API Key
+        Personal API Key
       </h5>
       <p className="italic text-sm mt-2 text-zinc-500 mb-6">
         Please note: This will be destroyed on logout
