222d2c8e087e534cc3a6eebd253ed0904c1afdb9
Martin Virando <martinvirando@hotmail.co.uk>
2022-03-21 10:03:54 +0000

    Fixed google analytics

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
@@ -23,11 +22,11 @@ class MyDocument extends Document {
           />
           {process.env.NODE_ENV === 'production' && (
             <>
-              <Script
+              <script
                 async
                 src="https://www.googletagmanager.com/gtag/js?id=UA-70478210-4"
               />
-              <Script
+              <script
                 dangerouslySetInnerHTML={{
                   __html: `
       window.dataLayer = window.dataLayer || [];
