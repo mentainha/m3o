ea333797acd6fd378ea630478f51c51e52be670b
Martin Virando <martinvirando@hotmail.co.uk>
2022-04-08 09:47:11 +0100

    Removed cloud

--- apps/web/components/layouts/DashboardLayout.tsx
+++ apps/web/components/layouts/DashboardLayout.tsx
@@ -140,31 +140,31 @@ export function DashboardLayout({
                   },
                 ],
               },
-              {
-                title: 'Cloud',
-                items: [
-                  {
-                    text: 'Apps',
-                    href: '/cloud/apps',
-                    icon: ServerIcon,
-                  },
-                  {
-                    text: 'Database',
-                    href: '/cloud/database',
-                    icon: DatabaseIcon,
-                  },
-                  {
-                    text: 'Functions',
-                    href: '/cloud/functions',
-                    icon: CodeIcon,
-                  },
-                  {
-                    text: 'Users',
-                    href: '/cloud/users',
-                    icon: UserGroupIcon,
-                  },
-                ],
-              },
+              // {
+              //   title: 'Cloud',
+              //   items: [
+              //     {
+              //       text: 'Apps',
+              //       href: '/cloud/apps',
+              //       icon: ServerIcon,
+              //     },
+              //     {
+              //       text: 'Database',
+              //       href: '/cloud/database',
+              //       icon: DatabaseIcon,
+              //     },
+              //     {
+              //       text: 'Functions',
+              //       href: '/cloud/functions',
+              //       icon: CodeIcon,
+              //     },
+              //     {
+              //       text: 'Users',
+              //       href: '/cloud/users',
+              //       icon: UserGroupIcon,
+              //     },
+              //   ],
+              // },
               {
                 title: 'Account',
                 items: [
