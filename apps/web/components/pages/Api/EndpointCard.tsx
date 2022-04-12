b636dcdfa2c17ed3fcea4c212dafca10aae25355
Asim Aslam <asim@aslam.me>
2022-04-12 09:21:15 +0100

    .

--- apps/web/components/pages/Api/EndpointCard.tsx
+++ apps/web/components/pages/Api/EndpointCard.tsx
@@ -38,18 +38,15 @@ export const EndpointCard: FC<Props> = ({
     <div className="endpoint-card" id={title.replace(/ /g, '')}>
       <div className="md:grid md:grid-cols-2">
         <div className="pb-4">
-          <p className="mb-6 font-bold text-sm text-indigo-300">
-            {price} {price === 'Free' ? '' : 'credits'}
-          </p>
+          <h3 className="font-medium text-black text-3xl dark:text-white mb-2">
+            {splitEndpointTitle(title)}
+          </h3>
           <h4 className="mb-4 inline-block text-xs">
             <span className="font-medium text-indigo-400">{apiMethod}</span>{' '}
             <span className="text-sm text-zinc-400">
               /{apiVersion}/{apiName}/{endpointName}
             </span>
           </h4>
-          <h3 className="font-medium text-black text-3xl dark:text-white mb-2">
-            {splitEndpointTitle(title)}
-          </h3>
           {requestSchema && (
             <p className="text-zinc-400 mb-10  max-w-md">
               {requestSchema.description}
