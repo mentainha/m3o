d438d59fe03b145be11adfa14a7eb7699ead0317
Asim Aslam <asim@aslam.me>
2022-04-13 08:42:19 +0100

    .

--- apps/web/components/ui/Subscriptions.tsx
+++ apps/web/components/ui/Subscriptions.tsx
@@ -11,7 +11,7 @@ export function Subscriptions(): ReactElement {
         plan={SubscriptionPlans.Free}
         description="For testing and hacking"
         features={[
-          '10k requests per month',
+          '10k requests for free',
           '1M requests per credit',
           '100 User accounts',
           '1000 DB records',
