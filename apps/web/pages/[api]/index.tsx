6f18a3b6c699d49a82aed5303da1ba1c9632dbfd
Asim Aslam <asim@aslam.me>
2022-04-20 11:26:02 +0100

    add a pricing tabe

--- apps/web/pages/[api]/index.tsx
+++ apps/web/pages/[api]/index.tsx
@@ -119,9 +119,27 @@ const Overview: NextPage<Props> = ({
                   {feature.title}
                 </a>
               </h3>
-              <p className="text-zinc-400 text-sm">{feature.description}</p>
+              <p className="text-zinc-400 text-sm max-w-lg">{feature.description}</p>
             </div>
           ))}
+          <h2 id="pricing" className="text-white text-xl mb-4 mt-10 font-bold">Pricing</h2>
+          <table className="min-w-full mb-6">
+            <thead className="mb-4">
+              <tr>
+                <th className="text-left font-normal">Endpoint</th>
+                <th className="text-left font-normal">Credits</th>
+              </tr>
+            </thead>
+            <tbody>
+          {features.map(feature => (
+              <tr className="border-t border-zinc-700">
+                <td className="py-2 w-10/12 text-sm">{feature.title}</td>
+                <td className="py-2 w-2/12 text-sm">{feature.price === 'Free' ? 'Base' : feature.price}</td>
+              </tr>
+          ))}
+            </tbody>
+          </table>
+          <p className="text-zinc-400 text-sm max-w-lg">Base request price is 0.000001 credits per request</p>
         </div>
         <div className="flex flex-col">
           <LinkButton
