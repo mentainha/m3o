b224d159daa2a9a7f51b8cd76c09db4f0573d29b
Asim Aslam <asim@aslam.me>
2022-04-24 10:39:56 +0100

    Update index.tsx

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -54,7 +54,7 @@ export default function Home({
         <LoggedInView user={user} apiToken={apiToken} />
       ) : (
         <Landing
-          heading="Universal public API gateway"
+          heading="Universal Public API Gateway"
           services={services}
           subHeading="Explore, discover and consume public APIs as simpler programmable
           building blocks all on one platform for a 10x developer experience."
