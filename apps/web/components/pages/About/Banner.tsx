f165750f0b474ec346c408d7af6277a8709b8aad
Asim Aslam <asim@aslam.me>
2022-04-27 14:31:35 +0100

    Update Banner.tsx (#239)

--- apps/web/components/pages/About/Banner.tsx
+++ apps/web/components/pages/About/Banner.tsx
@@ -8,11 +8,11 @@ export const Banner: FC = () => {
     <section className="py-12 md:py-16 border-b border-zinc-300 border-solid dark:bg-zinc-900 dark:border-zinc-600">
       <div className="md:max-w-4xl lg:max-w-7xl text-center ml-auto mr-auto w-11/12">
         <h1 className="font-bold text-4xl md:text-6xl mb-6 text-black dark:text-white">
-          Next Generation Cloud
+          Universal Public API Interface
         </h1>
         <h2 className="text-md md:text-lg max-w-xl mx-auto text-zinc-700 dark:text-zinc-400 font-medium">
-          Explore, discover and consume public APIs as simpler programmable building
-          blocks all on one platform for a 10x developer experience.
+          Explore, discover and consume public APIs as simple programmable building
+          blocks
         </h2>
         <div className="mt-6">
           <Link href={Routes.SignUp}>
