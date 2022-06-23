209fef77b9f85b75b1fad2660c43d671ab59da0c
Asim Aslam <asim@aslam.me>
2022-06-23 13:00:19 +0100

    .

--- apps/web/components/pages/Home/Pricing.tsx
+++ apps/web/components/pages/Home/Pricing.tsx
@@ -7,7 +7,7 @@ export const Pricing: FC = () => {
       <div className="m3o-container sm p-16 text-center">
         <h3 className="description-heading">Pricing</h3>
         <h4 className="text-4xl my-4 pb-2 text-white font-bold">
-          Our plans & subscriptions
+          Our Plans
         </h4>
         <Subscriptions />
       </div>
