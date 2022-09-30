10ca76ea9909d8bbff55581402ab694f4257582b
asim <asim@aslam.me>
2022-09-30 20:25:50 +0100

    remove uneven table for cloud/users

--- apps/web/components/pages/Cloud/Apps/UserDetailRow.tsx
+++ apps/web/components/pages/Cloud/Apps/UserDetailRow.tsx
@@ -9,7 +9,7 @@ export function UserDetailRow({
   children,
 }: PropsWithChildren<Props>): ReactElement {
   return (
-    <div className="grid grid-cols-2 p-3 text-sm even:bg-zinc-50 mb-4 rounded-md items-center">
+    <div className="grid grid-cols-2 p-3 text-sm rounded-md items-center">
       <p>{label}</p>
       <p>{children || '-'}</p>
     </div>
