7d55a8de9245c89ada302fa907b1b025a7bfb247
asim <asim@aslam.me>
2022-10-03 18:29:25 +0100

    .

--- functions/template/index.js
+++ functions/template/index.js
@@ -1,7 +1,7 @@
-import functions from '@google-cloud/functions-framework';
-import m3o from 'm3o';
-import { handler } from './handler/index.js';
+const functions = require('@google-cloud/functions-framework')
+const m3o = require('m3o').default
+const { handler } = require('./handler/index.js')
 
-global.m3o = m3o(process.env.NODE_ENV);
+global.m3o = m3o(process.env.NODE_ENV)
 
-functions.http('main', handler);
+functions.http('main', handler)
