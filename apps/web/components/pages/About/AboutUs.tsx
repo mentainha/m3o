c17ad4e5a6d6b355676c542164830ebcbd672173
asim <asim@aslam.me>
2022-11-01 13:42:40 +0000

    .

--- apps/web/components/pages/About/AboutUs.tsx
+++ apps/web/components/pages/About/AboutUs.tsx
@@ -11,22 +11,22 @@ export const AboutUs: FC = () => {
         <div className="md:grid md:grid-cols-1 m-auto max-w-md md:gap-4 text-zinc-800 font-light dark:text-zinc-300">
           <div className="mb-6 md:mb-0">
             <p className="mb-4">
-              M3O is a new cloud platform that focuses
+              M3O is a community led cloud platform that focuses
               entirely on the developer experience. We&apos;re building an AWS
               alternative for the next generation of developers.
             </p>
           </div>
           <div className="mb-6 md:mb-0">
             <p className="mb-4">
-              In 2013, at the ride hailing startup Hailo, we cut our teeth on 
-	      AWS and the cost of cloud complexity just to ship a consumer app.
+              A decade ago, at a ride hailing startup called Hailo, we cut our teeth on 
+	      AWS and the cost of cloud complexity just to ship a taxi app to end consumers.
             </p>
           </div>
           <div className="mb-6 md:mb-0">
             <p className="mb-4">
-              M3O started life as an open source project called Micro, to address
+              M3O started life as Micro, an open source project built to address
               those pains. Now we're leveraging it to offer Micro services as 
-	      programmable building blocks.
+	      reusable building blocks for everyone.
             </p>
           </div>
         </div>
