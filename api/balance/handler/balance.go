19f4635c262458ed6449d5d906ef2fe49a38b188
asim <asim@aslam.me>
2023-02-04 13:41:06 +0000

    lower margin

--- api/balance/handler/balance.go
+++ api/balance/handler/balance.go
@@ -131,7 +131,7 @@ func NewHandler(svc *service.Service) *Balance {
 	b := &Balance{
 		c:         &counter{redisClient: rc},
 		stripeSvc: stripe.NewStripeService("stripe", svc.Client()),
-		margin:    mval.Float64(1.0),
+		margin:    mval.Float64(0.25),
 	}
 	go b.consumeEvents()
 	return b
