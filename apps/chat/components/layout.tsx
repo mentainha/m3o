b86cd149ccfac9fd72762dc3734c8ceed38a6560
asim <asim@aslam.me>
2023-05-15 13:40:27 +0100

    update colours

--- apps/chat/components/layout.tsx
+++ apps/chat/components/layout.tsx
@@ -60,7 +60,7 @@ export default function Layout({
         <div className={styles.container}>
           <header className={styles.header}>
             <Link href="/">
-              <img src="/logo.png" height="100px" width="100px" alt="Logo" />
+              <img src="/logo.png" height="30px" width="30px" alt="Logo" />
             </Link>
 
             <nav>
